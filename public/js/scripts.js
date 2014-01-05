6f6256065933b728c92922e52393698c2bd3b523
Alan Mesquita <alan.mdesousa@gmail.com>
2014-01-05 13:21:04 -0200

    works and refactored

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,16 @@
 jQuery(function(){
-    alert('a');
+    $('.open-reply').on('click', function(){
+        $(this).closest('.post-cell').find('.post-reply').toggle();
+    })
+
+    $('form').on('submit', function(e){
+        var text = $(this).find('textarea');
+        if(text.val() == '') {
+            
+            e.preventDefault();
+            alert('Ops! Voce precisa deixar um comentario!')
+            text.css('border','1px solid red');
+        }
+    })
 });
+
